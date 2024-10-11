4dc4db0214e8884126c37890ee26dab4661be7a6
mint3382 <mskim0926@naver.com>
2024-10-11 15:04:34 +0900

    ⚙️(setting) UIKits 폴더 생성

---
+++ UIKits/UIKitsTests/UIKitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitsTests.swift
+//  UIKitsTests
+//
+//  Created by minsong kim on 10/11/24.
+//
+
+import Testing
+@testable import UIKits
+
+struct UIKitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
