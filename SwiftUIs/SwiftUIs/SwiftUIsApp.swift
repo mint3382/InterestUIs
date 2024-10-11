5d3bdede5278bf5f916993a8788741b20a7d9800
mint3382 <mskim0926@naver.com>
2024-10-11 15:05:35 +0900

    ⚙️(setting) SwiftUIs 폴더 생성

---
+++ SwiftUIs/SwiftUIs/SwiftUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIsApp.swift
+//  SwiftUIs
+//
+//  Created by minsong kim on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIs/SwiftUIsTests/SwiftUIsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIsTests.swift
+//  SwiftUIsTests
+//
+//  Created by minsong kim on 10/11/24.
+//
+
+import Testing
+@testable import SwiftUIs
+
+struct SwiftUIsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
